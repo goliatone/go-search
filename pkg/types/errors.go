e9c001b395db6402ba6d93f2cdd921815683e1b6
goliatone <140582+goliatone@users.noreply.github.com>
2026-03-20 16:48:27 -0700

    add: updated error definitions

--- pkg/types/errors.go
+++ pkg/types/errors.go
@@ -5,6 +5,7 @@ const (
 	TextCodeUnsupportedCapability = "SEARCH_UNSUPPORTED_CAPABILITY"
 	TextCodeInvalidFilter         = "SEARCH_INVALID_FILTER"
 	TextCodeInvalidSort           = "SEARCH_INVALID_SORT"
+	TextCodeInvalidEditorialRule  = "SEARCH_INVALID_EDITORIAL_RULE"
 	TextCodeIndexingSourceMissing = "SEARCH_INDEXING_SOURCE_NOT_REGISTERED"
 	TextCodeProjectorFailure      = "SEARCH_PROJECTOR_FAILURE"
 	TextCodeSchemaMismatch        = "SEARCH_SCHEMA_MISMATCH"
